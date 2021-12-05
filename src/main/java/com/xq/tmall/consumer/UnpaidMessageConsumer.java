92eb7f39d1ce77e27e1edca5a562927cb3097441
Edith990 <2226889040@qq.com>
2021-12-05 12:43:24 +0800

    update UnpaidMessageConsumer

--- src/main/java/com/xq/tmall/consumer/UnpaidMessageConsumer.java
+++ src/main/java/com/xq/tmall/consumer/UnpaidMessageConsumer.java
@@ -1,6 +1,9 @@
 package com.xq.tmall.consumer;
 
 import com.xq.tmall.message.Order;
+import com.xq.tmall.redis.GoodsKey;
+import com.xq.tmall.result.CodeMsg;
+import com.xq.tmall.result.Result;
 import com.xq.tmall.service.ProductOrderService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
@@ -8,8 +11,12 @@ import org.apache.rocketmq.spring.core.RocketMQListener;
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+import com.xq.tmall.redis.RedisService;
+import com.xq.tmall.result.CodeMsg;
+import java.util.HashMap;
+import com.xq.tmall.result.Result;
 @Component
 @RocketMQMessageListener(
         //        consumerGroup = "demo01-A-consumer-group-" + Demo01Message.TOPIC
@@ -18,11 +25,19 @@ import org.springframework.stereotype.Component;
 public class UnpaidMessageConsumer implements RocketMQListener<Order> {
     private Logger logger = LogManager.getLogger(UnpaidMessageConsumer.class);
     ProductOrderService productOrderService;
-
+    @Autowired
+    RedisService redisService;
+    private HashMap<Integer, Boolean> localOverMap = new HashMap<Integer, Boolean>();
     @Override
     public void onMessage(Order order) {
         //触发监听之后去查根据Order.orderId的订单编号去数据库/Redis缓存里查是否付款,没付款取消订单
         logger.info("延迟消息[onMessage][线程编号:{} 消息内容：{}]", Thread.currentThread().getId(), order);
-        productOrderService.deleteList(new Integer[]{Integer.valueOf(order.getOrderId())});
+        if (productOrderService.get(Integer.valueOf(order.getOrderId())).getProductOrder_status()==1) {
+            productOrderService.deleteList(new Integer[]{Integer.valueOf(order.getOrderId())});
+
+            long stock = redisService.decr(GoodsKey.getSeckillGoodsStock, "" + Integer.valueOf(order.getOrderId()));
+        }
+
+
     }
 }
